fd0557b28473bb444d828b274881e3a3c3b957cd
Felix Kipruto <kimosopfelix@gmail.com>
2022-11-22 11:12:50 +0300

    Completed message

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
 
 // Write your code here
+let num1 = 2
+let num2 = 31
+
+const multiply = num1*num2;
+
+multiply;
+
+
+let random;
+
+random = Math.floor(Math.random()*10)+1;
+
+random;
+
+let num3 = 20
+let num4 = 8
+
+const mod = num3 % num4;
+
+mod;
+
+let max;
+
+max = Math.max(15,16,17,18,19,20);
+
+max;
+
